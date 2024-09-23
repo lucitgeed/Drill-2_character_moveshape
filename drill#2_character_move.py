fd15c7f7748f84fad2800ded94401352107dd77a
homedesktop@2DGP <gunageroo@gmail.com>
2024-09-23 12:34:14 +0900

    Update drill#2_character_move.py

--- drill#2_character_move.py
+++ drill#2_character_move.py
@@ -6,7 +6,7 @@ grass = load_image('grass.png')
 char = load_image('character.png')
 
 
-grass.draw_now(400, 90)
+grass.draw_now(400, 30)
 char.draw_now(400, 90)
 
 
@@ -14,26 +14,31 @@ while(True):
 
     for i in range(0, 200):
         clear_canvas_now()
+        grass.draw_now(400, 30)
         char.draw_now(400 + i, 90)
         i = i + 2
 
-    for i in range(0, 100):
+    for i in range(0, 200):
         clear_canvas_now()
+        grass.draw_now(400, 30)
         char.draw_now(400 + 200, 90 + i)
         i = i+2
 
     for i in range(0,400):
         clear_canvas_now()
-        char.draw_now(400 + 200 - i, 90 + 100)
+        grass.draw_now(400, 30)
+        char.draw_now(400 + 200 - i, 90 + 200)
         i = i+2
 
-    for i in range(0, 100):
+    for i in range(0, 200):
         clear_canvas_now()
-        char.draw_now(400 + 200 - 400, 90 + 100 - i)
+        grass.draw_now(400, 30)
+        char.draw_now(400 + 200 - 400, 90 + 200 - i)
         i = i + 2
 
     for i in range(0, 200):
         clear_canvas_now()
+        grass.draw_now(400, 30)
         char.draw_now(400 + 200 - 400 + i, 90)
         i = i + 2
 
@@ -44,29 +49,32 @@ while(True):
 
     for x in range(0, 50):
             clear_canvas_now()
+            grass.draw_now(400, 30)
             char.draw_now(400 + x, 90 + x)
             x = x + 2
 
     for x in range(0, 50):
             clear_canvas_now()
+            grass.draw_now(400, 30)
             char.draw_now(400 + 50 - x, 90 + 50 + x)
             x = x + 2
 
 
     for x in range(0, 50):
             clear_canvas_now()
+            grass.draw_now(400, 30)
             char.draw_now(400 - x, 90 + 50 + 50 - x)
             x = x + 2
 
 
     for x in range(0, 50):
             clear_canvas_now()
+            grass.draw_now(400, 30)
             char.draw_now(400 - 50 + x, 90 + 50 - x)
             x = x + 2
 
+    delay(2)
 
-            
-    
 
 
 close_canvas()
